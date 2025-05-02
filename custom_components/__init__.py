432565da0207c71042df72516616055a00f9fe3e
hanwg <han.wuguang@gmail.com>
2025-05-02 07:24:41 +0000

    reorg folder

---
+++ custom_components/__init__.py
@@ -0,0 +1,43 @@
+"""The SG Bus Arrivals integration."""
+
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_API_KEY, Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryAuthFailed
+
+from . import sg_bus_arrivals_service
+
+_PLATFORMS: list[Platform] = [Platform.SENSOR]
+
+type SgBusArrivalsConfigEntry = ConfigEntry[
+    sg_bus_arrivals_service.SgBusArrivalsService
+]
+
+
+async def async_setup_entry(
+    hass: HomeAssistant, entry: SgBusArrivalsConfigEntry
+) -> bool:
+    """Set up SG Bus Arrivals from a config entry."""
+
+    # create instance of our api
+    service = sg_bus_arrivals_service.SgBusArrivalsService(
+        hass, entry.data[CONF_API_KEY]
+    )
+
+    result = await service.authenticate()
+    # if not result:
+    raise ConfigEntryAuthFailed
+
+    entry.runtime_data = service
+    await hass.config_entries.async_forward_entry_setups(entry, _PLATFORMS)
+
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant, entry: SgBusArrivalsConfigEntry
+) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, _PLATFORMS)
---
+++ custom_components/config_flow.py
@@ -0,0 +1,103 @@
+"""Config flow for the SG Bus Arrivals integration."""
+
+from __future__ import annotations
+
+from collections.abc import Mapping
+import logging
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant.config_entries import SOURCE_REAUTH, ConfigFlow, ConfigFlowResult
+from homeassistant.const import CONF_API_KEY
+from homeassistant.core import HomeAssistant
+
+from . import sg_bus_arrivals_service
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+STEP_USER_DATA_API_SCHEMA = vol.Schema({vol.Required(CONF_API_KEY): str})
+
+
+async def validate_api(
+    hass: HomeAssistant, data: dict[str, Any], errors: dict[str, str]
+) -> None:
+    """Validate the user input allows us to connect.
+
+    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
+    """
+
+    service = sg_bus_arrivals_service.SgBusArrivalsService(hass, data[CONF_API_KEY])
+
+    try:
+        if not await service.authenticate():
+            errors["base"] = "invalid_auth"
+    except Exception:
+        _LOGGER.exception("Unexpected exception")
+        errors["base"] = "unknown"
+
+    return errors
+
+
+class ConfigFlow(ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for SG Bus Arrivals."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle the initial step."""
+        errors: dict[str, str] = {}
+        if user_input is not None:
+            await validate_api(self.hass, user_input, errors)
+            if not errors:
+                if self.source == SOURCE_REAUTH:
+                    return self.async_update_reload_and_abort(
+                        self._get_reauth_entry(), data_updates=user_input
+                    )
+
+                return self.async_create_entry(
+                    title="LTA DataMall API", data=user_input
+                )
+
+        return self.async_show_form(
+            step_id="user", data_schema=STEP_USER_DATA_API_SCHEMA, errors=errors
+        )
+
+    async def async_step_reconfigure(self, user_input: dict[str, Any] | None = None):
+        """Re-authenticate API."""
+
+        errors: dict[str, str] = {}
+        if user_input is not None:
+            self._abort_if_unique_id_mismatch()
+
+            await validate_api(self.hass, user_input, errors)
+
+            if not errors:
+                return self.async_update_reload_and_abort(
+                    self._get_reconfigure_entry(), data_updates=user_input
+                )
+
+        return self.async_show_form(
+            step_id="reconfigure", data_schema=STEP_USER_DATA_API_SCHEMA, errors=errors
+        )
+
+    async def async_step_reauth(
+        self, entry_data: Mapping[str, Any]
+    ) -> ConfigFlowResult:
+        """Perform reauth upon an API authentication error."""
+        return await self.async_step_reauth_confirm()
+
+    async def async_step_reauth_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Dialog that informs the user that reauth is required."""
+
+        if user_input is None:
+            return self.async_show_form(
+                step_id="reauth_confirm",
+                data_schema=STEP_USER_DATA_API_SCHEMA,
+            )
+        return await self.async_step_user()
